7f9bc8485c70e2fd943e68b2cb60c32021bb520b
Jules Blom <julesblom@gmail.com>
2026-03-03 09:59:49 +0100

    Remove bro/xml demo

--- public/bro-xml-parser/adapters/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * XML adapter exports
- *
- * This module exports the adapter classes for advanced use cases.
- * Most users should import from the browser or node entry points instead.
- */
-export { BrowserXMLAdapter } from './browser-adapter.js';
-export { NodeXMLAdapter } from './node-adapter.js';
-//# sourceMappingURL=index.js.map
--- public/bro-xml-parser/index.js
+++
@@ -1,5 +0,0 @@
-/** Default entry point is the browser implementation.
- * For Node.js, use: import { BROParser } from 'bro-parser/node'
- */
-export * from './browser.js';
-//# sourceMappingURL=index.js.map
